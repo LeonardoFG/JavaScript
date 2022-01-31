ad376db262090a21a11e1c00209921d333a26a46
Leonardo FG <leonardo.fg@engineer.com>
2022-01-31 06:31:59 -0300

    Update principal.js

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -4,5 +4,8 @@ titulo.textContent = "Aparecida Nutricionista";
 var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 
+var peso = tdPeso.textContent;
+
 console.log(paciente); // tr
 console.log(tdPeso); // td que tem o peso
+console.log(peso); //Obter 100
