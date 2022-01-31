816e0de07a448bd0e7bb9e784c8b9c923abc3e2a
Leonardo FG <leonardo.fg@engineer.com>
2022-01-31 06:38:17 -0300

    Update principal.js

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -9,6 +9,6 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-console.log(paciente); // tr
-console.log(tdAltura); 
-console.log(altura); 
+var imc = peso / altura * altura; // 100 / 2.0 x 2.0 = 100 / 4 =>>>>>>> 25
+
+console.log(imc);
