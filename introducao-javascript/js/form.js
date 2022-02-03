da58d7f6b25943158404a314f1d4dc6fe409b7c0
Leonardo FG <leonardo.fg@engineer.com>
2022-02-03 08:19:19 -0300

    Update form.js

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -9,25 +9,11 @@ botaoAdicionar.addEventListener("click", function(event) {
     console.log(paciente);
 
     //cria a tr e a td do paciente
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    function montaTr(paciente) {
+        var pacienteTr = document.createElement("tr");
+    
+        return pacienteTr;
+    }
 
     //adicionando o paciente na tabela.
     var tabela = document.querySelector("#tabela-pacientes");
