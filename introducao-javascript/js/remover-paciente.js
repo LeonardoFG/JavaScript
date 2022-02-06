39931af9429f2ff532fd5635d0833fa82370f59d
Leonardo FG <leonardo.fg@engineer.com>
2022-02-06 14:35:12 -0300

    Update remover-paciente.js

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -2,8 +2,10 @@ var pacientes = document.querySelectorAll(".paciente");
 
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("click", function() {
-    console.log("Fui clicado");
+tabela.addEventListener("dblclick", function(event) {
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover 
+    paiDoAlvo.remove();
 });
 
 /*pacientes.forEach(function(paciente) {
