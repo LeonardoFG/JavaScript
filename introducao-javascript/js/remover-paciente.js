8570286ff3628c1145f23b58ec3b40ef4440e4b1
Leonardo FG <leonardo.fg@engineer.com>
2022-02-06 13:35:08 -0300

    Update remover-paciente.js

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -2,6 +2,6 @@ var pacientes = document.querySelectorAll(".paciente");
 
 pacientes.forEach(function(paciente) {
     paciente.addEventListener("dblclick", function() {
-
+        this.remove();
     });
 });
