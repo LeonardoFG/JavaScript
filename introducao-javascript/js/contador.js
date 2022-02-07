41e5839a1fcab4e5ee9d3db916eee253f25cc700
Leonardo FG <leonardo.fg@engineer.com>
2022-02-07 14:14:18 -0300

    Create contador.js

---
+++ introducao-javascript/js/contador.js
@@ -0,0 +1,9 @@
+// contador.js
+
+function atualizaCaracteres() {
+    var postagem = document.querySelector("#corpo-postagem").value;
+    var caracteres = postagem.length;
+
+    var contador = document.querySelector("#numero-caracteres");
+    contador.innerHTML = caracteres;
+}
