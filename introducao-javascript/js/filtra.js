c905c37130209f051d9b821df52129a75c3cf6d5
Leonardo FG <leonardo.fg@engineer.com>
2022-02-07 14:01:08 -0300

    Update filtra.js

--- introducao-javascript/js/filtra.js
+++ introducao-javascript/js/filtra.js
@@ -1,5 +1,5 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
-    console.log("Digitaram no campo");
+    console.log(campoFiltro.value);
 });
