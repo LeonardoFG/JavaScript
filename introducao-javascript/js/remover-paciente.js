ae59a5b91e5008497fad19f9eb80de1d60d88210
Leonardo FG <leonardo.fg@engineer.com>
2022-02-07 14:53:33 -0300

    Update remover-paciente.js

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -4,19 +4,16 @@ var tabela = document.querySelector("table");
 
 const linhas = document.querySelectorAll('table tbody tr') // linhas, imposibilita a tabela inteira ser removida
 const colunas = document.querySelectorAll('table tbody tr td') // colunas, imposibilita a tabela inteira ser removida
+const cabecalho = document.querySelectorAll('table thead tr th') // thead, imposibilita a tabela inteira ser removida
 
 tabela.addEventListener("dblclick", function(event) {
-    if (event.target.parentNode === "#headtabela"){
-        return;
-        
-    } else {
+    if(tabela != linhas && tabela != colunas && tabela != cabecalho){
         event.target.parentNode.classList.add("fadeOut");
-        
+            
         setTimeout(function() {
             event.target.parentNode.remove();
         }, 500); //faz a função remove aguardar 0,5s para dar para ver a animação
     }
-
 });
 
 /*pacientes.forEach(function(paciente) {
