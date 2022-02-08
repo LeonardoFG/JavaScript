1ea5774de42a19480d3a60545f9519b661f626c6
Leonardo FG <leonardo.fg@engineer.com>
2022-02-08 12:56:10 -0300

    checa retorno 200

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -1,20 +1,23 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
 botaoAdicionar.addEventListener("click", function(){
-    console.log("Buscando pacientes...");
-
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function() {
-        var resposta = xhr.responseText;
 
-        var pacientes = JSON.parse(resposta);
+        if (xhr.status == 200) { //checa se a importação deu certo
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
     });
 
     xhr.send();
